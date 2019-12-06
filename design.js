403a0da4dbe940f03afeb33304adc1309e23e3f1
jasminebanger <37240370+jasminebanger@users.noreply.github.com>
2019-12-06 23:06:08 +0530

    Update design.js

--- design.js
+++ design.js
@@ -1,6 +1,6 @@
 //for showing a div while clicking on read more button
 function myFunction() {
   document.getElementById("block-div").style.display = "block";
-  document.getElementById("header").style.filter="blur(4px)";
+  document.getElementById("header").style.filter = "blur(4px)";
 }
  
