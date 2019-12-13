d06b503bc64086e83c342eec1a143dd8a7c6d8f3
jasminebanger <37240370+jasminebanger@users.noreply.github.com>
2019-12-13 20:59:22 +0530

    Update design.js

--- design.js
+++ design.js
@@ -1,4 +1,5 @@
 //for showing a div while clicking on read more button
+// myFunction() for block-div 
 function myFunction() {
   document.getElementById("block-div").style.display = "block";
   document.getElementById("header").style.filter = "blur(4px)";
@@ -7,7 +8,7 @@ function nanFunction(){
   document.getElementById("block-div").style.display = "none";
   document.getElementById("header").style.filter="none";
 }
-
+// myFunction1() for block-div1
 function myFunction1(){
   document.getElementById("block-div1").style.display = "block";
   document.getElementById("header").style.filter="blur(4px)";
@@ -16,7 +17,7 @@ function nanFunction1(){
   document.getElementById("block-div1").style.display = "none";
   document.getElementById("header").style.filter="none";
 }
-
+// myFunction2() for block-div2
 function myFunction2() {
   document.getElementById("block-div2").style.display = "block";
   document.getElementById("header").style.filter="blur(4px)";
@@ -25,7 +26,7 @@ function nanFunction2(){
   document.getElementById("block-div2").style.display = "none";
   document.getElementById("header").style.filter="none";
 }
-
+// myFunction3() for block-div3
 function myFunction3() {
   document.getElementById("block-div3").style.display = "block";
   document.getElementById("header").style.filter="blur(4px)";
